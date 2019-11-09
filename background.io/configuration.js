9da32b36e19ef1f4aaad546afef2b3c06498ac97
Pramod singh <pmdsingh50@gmail.com>
2019-11-09 21:51:05 +0530

    lint changes

--- background.io/configuration.js
+++ background.io/configuration.js
@@ -1,2 +1,2 @@
-module.exports.MAX_INTERVAL_CHECK = 15000; //15 sec for each xpath check
-module.exports.MAX_MONITORING_TIME = 1800000; //30 min after each socket request
+module.exports.MAX_INTERVAL_PER_XPATH = 10000 // 10 sec for each xpath check
+module.exports.MAX_MONITORING_TIME = 50000 // 30 min (1800000) after each socket request
--- background.io/index.js
+++ background.io/index.js
@@ -1,70 +1,74 @@
-const io = require('socket.io')();
-const puppeteer = require('puppeteer');
-const config = require('./configuration');
-//data format
-// const mockData = [
-//     {
-//         url: 'https://web-qe.marketo.com/index.html?w=1#/ds/file/49726',
-//         xpath: '/html/body/div[4]/div/div[3]/div/div[1]/div/div/div[2]/div/div/div[2]/div[1]/div[2]/label/div/input',
-//         value: 'ghjgjgjghgjh',
-//     }
-// ]
-io.listen(5000);
-io.on('connection', (socket) => {
-    console.log("connected");
-    socket.on('watchMeRequest', recordList => {
-        //check for websites and emit the valid response
-        // requestHandler(recordList)
-    });
-});
+const io = require('socket.io')()
+const puppeteer = require('puppeteer')
+const config = require('./configuration')
 
-const requestHandler = async () => {
-    const recordList = [...mockData]; // for now mock data
-    const {page, browser} = await initiateLogin();
-    const processEachRecordInterval = setInterval( async () => await processEachRecord({page, browser, recordList}), config.MAX_INTERVAL_CHECK);
-    setTimeout(() => {
-        clearInterval(processEachRecordInterval);
-        browser.close();
-    }, config.MAX_MONITORING_TIME);
+io.listen(5000)
+io.on('connection', (client) => {
+  client.on('watchMeRequest', ({clientID, data}, fn) => {
+    fn("connection-received");
+    console.log(data)
+    if(data.length !== 0){
+        requestHandler({clientID, recordList: data})
+    }
+  })
+})
+
+const requestHandler = async ({ clientID, recordList=[] }) => {
+  // do login
+  const { page, browser } = await initiateLogin() 
+  console.log("LOGIN DONE");
+  //time calculation
+  const RECORLIST_REPEAT_INTERVAL = config.MAX_INTERVAL_PER_XPATH * recordList.length;
+  const MAX_MONITORING_TIME = (RECORLIST_REPEAT_INTERVAL > config.MAX_MONITORING_TIME / 5) ? RECORLIST_REPEAT_INTERVAL * 5 : config.MAX_MONITORING_TIME;
+  
+  const processEachRecordInterval = setInterval(async () => {
+    await processEachRecord({ page, browser, recordList, clientID })
+  }, RECORLIST_REPEAT_INTERVAL)
+  
+  setTimeout(() => {
+    clearInterval(processEachRecordInterval)
+    browser.close()
+  }, MAX_MONITORING_TIME)
 }
 
-const processEachRecord = async ({ page, recordList= [] }) => {
-    for(let i=0; i< recordList.length; i++){
-        const {value, xpath, url} = recordList[i];
-        await automateUrlAndGetValueByXpath({ page, url, xpath, value});
-    }
+const processEachRecord = async ({ page, recordList = [], clientID }) => {
+  for (let i = 0; i < recordList.length; i++) {
+    const record = recordList[i]
+    await automateUrlAndGetValueByXpath({ page, clientID, ...record })
+  }
 }
 
 const initiateLogin = async () => {
-    const username = 'admin.rubiks06012017@marketo.com';
-    const password = 'Bryc3.n311ian';
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 926 });
-    await page.goto('https://app-sjqe.marketo.com/#MM0A1', { waitUntil: 'networkidle0' });
-    await page.type('#loginUsername', username);
-    await page.type('#loginPassword', password);
-    await Promise.all([
-        page.click('#loginButton'),
-        page.waitForNavigation({ waitUntil: 'networkidle0' }),
-    ]);
-    return { page, browser }
+  const username = 'admin.rubiks06012017@marketo.com'
+  const password = 'Bryc3.n311ian'
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1920, height: 926 })
+  await page.goto('https://app-sjqe.marketo.com/#MM0A1', { waitUntil: 'networkidle0' })
+  await page.type('#loginUsername', username)
+  await page.type('#loginPassword', password)
+  await Promise.all([
+    page.click('#loginButton'),
+    page.waitForNavigation({ waitUntil: 'networkidle0' })
+  ])
+  return { page, browser }
 }
-const automateUrlAndGetValueByXpath = async ({page, url, xpath, value}) => {
-    //"AuthenticationErrorModal_visitLogin" might be using this name later !not imp
-    //got to url
-    await page.goto(url, { waitUntil: 'networkidle0' });
-    //wait for xpath to load
-    await page.waitForXPath(xpath, 5000);
-    //getting value from xpath
-    if(xpath.match('.*input')){
-        xpath += '/@value';
-    }
-    let content = await page.$x(xpath);
-    let text = await page.evaluate(element => element.textContent, content[0]);
-    console.log(text)
-    if(text && text != value){
-        console.log("value changed from " + value + " to " + text);
-    }
+const automateUrlAndGetValueByXpath = async ({ page, url, xpath, value, nid, clientID="" }) => {
+  // "AuthenticationErrorModal_visitLogin" might be using this name later !not imp
+  // got to url
+  await page.goto(url, { waitUntil: 'networkidle0' })
+  // wait for xpath to load
+  await page.waitForXPath(xpath, 5000)
+  // getting value from xpath
+  if (xpath.match('.*input')) {
+    xpath += '/@value'
+  }
+  const content = await page.$x(xpath)
+  const text = await page.evaluate(element => element.textContent, content[0])
+  if (text && text !== value) {
+    console.log('value changed from ' + value + ' to ' + text)
+    io.emit(`watchMeResponse-${clientID}`, {
+        clientID, nid, url, xpath, value, newValue: text
+    });
+  }
 }
-
